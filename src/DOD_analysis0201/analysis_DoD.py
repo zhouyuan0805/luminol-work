cb29a7edebb5f1d8a8a55d6d02e43df5502584f2
zhouyuan0805 <1003132805@qq.com>
2018-02-01 11:28:34 +0800

    upload analysis data

---
+++ src/DOD_analysis0201/analysis_DoD.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python  
+# encoding: utf-8  
+
+""" 
+@version: v1.0 
+@author: ZhouYuan
+@software: PyCharm Community Edition 
+@file: analysis_DoD.py 
+@time: 2018/2/1 11:02 
+"""
+
+
+def func():
+    pass
+
+
+
+if __name__ == "__main__":
+    pass
---
+++ src/TJ_detect/test.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+
+import time
+import datetime
+
+for i in range(0,10):
+    print(datetime.datetime.now())
+    time.sleep(10)
